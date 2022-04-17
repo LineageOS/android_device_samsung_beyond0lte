5a04f894de6251b8629d7477a1eccfc3a856a3e5
Tim Zimmermann <tim@linux4.de>
2022-04-17 20:45:54 +0200

    beyond0lte: Patch audio HAL to load audioparams shim
    Change-Id: I2e3647b0dee6b6899dd2d73abb511c3d45a96bdf

--- extract-files.sh
+++ extract-files.sh
@@ -8,6 +8,10 @@
 
 function blob_fixup() {
     case "${1}" in
+        vendor/lib/hw/audio.primary.exynos9820.so)
+            "${PATCHELF}" --add-needed libshim_audioparams.so "${2}"
+            sed -i 's/str_parms_get_str/str_parms_get_mod/g' "${2}"
+            ;;
         vendor/lib64/libexynoscamera3.so)
             xxd -p "${2}" | sed "s/8b022036/1f2003d5/g" | xxd -r -p > "${2}".patched
             mv "${2}".patched "${2}"
