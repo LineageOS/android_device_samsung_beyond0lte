1ca690e3dfbebebcb0444916aa19fe72d40d652c
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    beyond0lte: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- lineage_beyond0lte.mk
+++ lineage_beyond0lte.mk
@@ -17,6 +17,7 @@
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 ## Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
