fe55281644d8926290490c1b0829c61c938c9895
Tim Zimmermann <tim@linux4.de>
2023-10-23 07:27:05 +0200

    beyond0lte: Merge mixer_paths and mixer_gains into one file
    * Stock libaudioroute blob supports including a separate mixer_gains.xml file
      while AOSP one does not
    
    Change-Id: I874fb55891f99971ae4a4a226c131af236d7e08e

--- device.mk
+++ device.mk
@@ -25,7 +25,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/mixer_gains.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_gains.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Fingerprint Gestures
